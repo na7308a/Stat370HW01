736b2ac7435671541e1f6249d4db32bdd6a0195f
Nana-Ama Anane <na7308a@american.edu>
2017-02-14 12:46:47 -0500

    committ hw1 up to number 12

--- qscript.R
+++ qscript.R
@@ -1,3 +1,10 @@
 quad<-function(x,a=0,b=0,c=0){
   return(a*x^2+b*x+c)
 }
+x <- seq(from=-10, to =10, by=.1)
+x <- seq(from=-10, to =10, by=.1)
+quad(x,1,2,3)
+y2 <- quad(x,5,12,18)
+lines(x,y2)
+y3 <-quad(x,20,6,7)
+
